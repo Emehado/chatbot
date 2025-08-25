c590b6b2376509bedaa279d9a7fe96930c4f5c86
Bright Larson Nanevie <emehado@gmail.com>
2025-08-25 11:18:51 +0200

    connect FE and BE

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -10,6 +10,10 @@ app.get("/", (req: Request, res: Response) => {
   res.send("hello world");
 });
 
+app.get("/api/hello", (req, res) => {
+  res.json({ message: "Hello World" });
+});
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
